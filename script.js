a4d95a42706c2ed8a131d6633dfc7fe9d31f9cd7
Jobin John <jobinjohn664@gmail.com>
2021-06-01 12:44:31 +0530

    form submition done

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ var typed = new Typed(".typing", {
     loop: true
 });
 
+$("#submit-form").submit((e)=>{
+    e.preventDefault()
+    $.ajax({
+    url:"https://script.google.com/macros/s/AKfycbxdqcoLxkFB8nZMUnELHlK62cEEW5rC6ScalDdZC5qBI9ofjQW3khIe4IAZ7DNy-0Z-/exec",
+    data:$("#submit-form").serialize(),
+    method:"post",
+    success:function (response){
+        alert("Form submitted successfully")
+        window.location.reload()
+        //window.location.href="https://google.com"
+        },
+        error:function (err){
+        alert("Something Error")
+        }
+    })
+})
